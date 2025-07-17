b2218f29d864efabefe8f68d646556cd1e1e5036
taniguchi-rio <ice24212421@gmail.com>
2025-07-17 16:28:02 +0900

    会社の基本情報表示機能

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\TermController;
+use App\Models\Company;
+use App\Models\Term;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +30,9 @@ Route::group(['middleware' => 'guest:admin'], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
 
     Route::resource('restaurants', RestaurantController::class)->only(['index','show']);
+
+    Route::get('company', [CompanyController::class, 'index'])->name('company.index');
+    Route::get('terms', [TermController::class, 'index'])->name('terms.index');
 
     Route::group(['middleware' => ['auth', 'verified']], function () {
         Route::resource('user', UserController::class)->only(['index', 'edit', 'update']);
